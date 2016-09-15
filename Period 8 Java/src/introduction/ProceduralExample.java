982d201920f958c447c7ac0eae3c2055ac470ed2
Student8 <Student8@10.8.33.166>
2016-09-15 13:41:13 -0400

    Name thing

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -2,7 +2,7 @@ package introduction;
 
 public class ProceduralExample {
   public static void main(String[] args){
-    System.out.println("Hello, World!");
-    System.out.println("Hello, Mi Amore!");
+	String[] names ={"SwagboySyed","Bob","Rob"}; 
+    System.out.println("Hi, my name is "+names[0]+".");
   }
 }
