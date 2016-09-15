f8e2ec1c16cf717ffbf3a9cfb68dbc5f5e745b1b
Student8 <Student8@10.8.33.166>
2016-09-15 13:56:17 -0400

    Memes arent just dreams

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -3,7 +3,9 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
 	String[] names ={"SwagboySyed","Bob","Rob"};
-	String[] years ={"Senior","Junior","Sophomore","Freshman"}; 
+	String[] years ={"Senior","Junior","Sophomore",}; 
     System.out.println("Hi, my name is "+names[0]+" and i'm a "+years[0]+"!");
+    System.out.println("Hi, my name is "+names[1]+" and i'm a "+years[2]+"!");
+    System.out.println("Hi, my name is "+names[2]+" and i'm a "+years[1]+"!");
   }
 }
