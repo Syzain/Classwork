fe8ea4daf6ecefb563c9bce55100964406782021
Syzain <shussain6399@gmail.com>
2016-09-15 13:48:31 -0400

    3 lit 4 werds

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -4,6 +4,6 @@ public class ProceduralExample {
   public static void main(String[] args){
 	String[] names ={"SwagboySyed","Bob","Rob"};
 	String[] years ={"Senior","Junior","Sophomore","Freshman"}; 
-    System.out.println("Hi, my name is "+names[0]+" and i'm a"+years[0]+"!");
+    System.out.println("Hi, my name is "+names[0]+" and i'm a "+years[0]+"!");
   }
 }
