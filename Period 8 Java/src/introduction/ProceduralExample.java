c2ec5ca88110649fccfaedc298dd9c9bee1be8f6
Syzain <shussain6399@gmail.com>
2016-09-15 13:47:49 -0400

    Now it's not lame

--- Period 8 Java/src/introduction/ProceduralExample.java
+++ Period 8 Java/src/introduction/ProceduralExample.java
@@ -3,7 +3,7 @@ package introduction;
 public class ProceduralExample {
   public static void main(String[] args){
 	String[] names ={"SwagboySyed","Bob","Rob"};
-	String[] years ={"Senior,Junior,Sophomore,Freshman"}; 
+	String[] years ={"Senior","Junior","Sophomore","Freshman"}; 
     System.out.println("Hi, my name is "+names[0]+" and i'm a"+years[0]+"!");
   }
 }
