c18b6d320fe6ffb1c3e7536074186f10f2005b14
Student8 <Student8@10.8.33.166>
2016-10-18 14:02:53 -0400

    Littt

--- Period 8 Java/src/arrays/ArrayPractice.java
+++ Period 8 Java/src/arrays/ArrayPractice.java
@@ -6,16 +6,73 @@ public class ArrayPractice {
 
 	public static void main(String[] args){
 		long currentTime = System.currentTimeMillis();
-		initializingArraysExample();
-		long endTime = System.currentTimeMillis();
 		
+		int[] fiftyNumbers = new int[50];
+		populate(fiftyNumbers);
+		print(fiftyNumbers);
+		randomize(fiftyNumbers);
+		print(fiftyNumbers);
+		rollDice(fiftyNumbers);
+		countResults(fiftyNumbers);
+		
+		long endTime = System.currentTimeMillis();
+
+		System.out.println("The process took "+(endTime-currentTime+"ms."));
+	}
+	private static void countResults(int[] fiftyNumbers) {
+		int[] counter = new int[12];
+		for(int n:fiftyNumbers){
+			counter[n-1] = counter[n-1] + 1;
+		}
+		for(int i = 0; i< counter.length; i++){
+			System.out.println((i+1) + " was rolled "
+			+ 2*counter[i]/100 + " percent of the time");
+		}
+		
+	}
+	private static void rollDice(int[] fiftyNumbers) {
+		for(int i = 0; i< fiftyNumbers.length; i++){
+			double random = Math.random();
+			int roll = (int) (6*random);
+			roll++;
+			double random2 = Math.random();
+			int roll2 = (int) (6*random2);
+			roll2++;
+			fiftyNumbers[i] = (int) (roll+roll2);
+			System.out.println("#"+(i+1)+" "+fiftyNumbers[i]);
+			
+		}
+	}
+	private static void randomize(int[] fiftyNumbers) {
+		for(int i = 0; i< fiftyNumbers.length; i++){
+			fiftyNumbers[i] = (int) (Math.random() * 50);
+		}
+	}
+	private static void print(int[] fiftyNumbers) {
+		for(int i = 0; i< fiftyNumbers.length; i++){
+			System.out.println(fiftyNumbers[i]);
+		}
+		
+	}
+	private static void populate(int[] fiftyNumbers) {
+		for(int i = 0; i< fiftyNumbers.length; i++){
+			int string=(i+1);
+			fiftyNumbers[i] = string;
+		}
+		
+	}
+	private void demonstratePassByValue(){	
 		String[] someStrings = new String[1000];
 		standardPopulate(someStrings);
 		String s = someStrings[999];
 		makeSpecial(s);
+		someStrings[999] = getASpecialString();
 		print(someStrings);
 		
-		System.out.println("The process took "+(endTime-currentTime+"ms."));
+	}
+	private static String getASpecialString(){
+		String s = "This string is pretty special";
+		return s;
 	}
 	private static void makeSpecial(String s) {
 		s = "This string is special to my heart";
