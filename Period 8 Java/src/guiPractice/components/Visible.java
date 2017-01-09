0a82d73ab43efa8abbc88a44442772ce0f71a18b
Syzain <shussain6399@gmail.com>
2017-01-09 11:29:00 -0500

    Update Visible.java
    meeeeeemeeeeee

--- Period 8 Java/src/guiPractice/components/Visible.java
+++ Period 8 Java/src/guiPractice/components/Visible.java
@@ -7,9 +7,11 @@ public interface Visible {
 	public BufferedImage getImage();
 	public int getX();
 	public int getY();
+	public void setX(int x);
+	public void setY(int y);
 	public int getWidth();
 	public int getHeight();
-	public boolean isAnimated();
 	public void update();
+	public boolean isAnimated();
 	
 }
